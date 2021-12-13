8a23345aee763af8bce796a3bcdb6467a44eb0c9
Bruno Ricardo de Meira Ferreira <contato@brunoricardo.com.br>
2021-12-13 15:15:29 -0300

    Desafio resolvido

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -2,16 +2,22 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  name: string;
-  email: string;
+    name: string;
+    email: string;
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ email, name }: IRequest): User {
+        const userAlreadyExists = this.usersRepository.findByEmail(email);
+
+        if (userAlreadyExists) {
+            throw new Error("This e-mail is already registered!");
+        }
+
+        return this.usersRepository.create({ name, email });
+    }
 }
 
 export { CreateUserUseCase };
--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -1,13 +1,23 @@
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
 
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+    constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.params;
+        try {
+            const user = this.showUserProfileUseCase.execute({
+                user_id,
+            });
+            return response.json(user);
+        } catch (error) {
+            return response.status(404).json({
+                error: "This user doens't exists!",
+            });
+        }
+    }
 }
 
 export { ShowUserProfileController };
